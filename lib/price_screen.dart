1d6e4949a47b22bd3ba14de8e22cea4dd1471d32
Derik <derik@oagro.com.br>
2020-11-18 11:57:39 -0300

    Adjusting price_screen to show card with values correctly
     - And Creating getPickerItems method;
     - And Creating setRates method;

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'coin_data.dart';
+import 'network.dart';
+import 'crypto.dart';
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,6 +10,11 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedPicker = currenciesList[0];
+  String bitCoinRate;
+  String ethRate;
+  String ltcRate;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,34 +27,52 @@ class _PriceScreenState extends State<PriceScreen> {
         children: <Widget>[
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Crypto(text: cryptoList[0], rate: bitCoinRate, selectedPicker: selectedPicker).cryptoCard(),
+                Crypto(text: cryptoList[1], rate: ethRate, selectedPicker: selectedPicker).cryptoCard(),
+                Crypto(text: cryptoList[2], rate: ltcRate, selectedPicker: selectedPicker).cryptoCard(),
+              ],
+            )
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: CupertinoPicker(
+              itemExtent: 32.0,
+              backgroundColor: Colors.lightBlue,
+              onSelectedItemChanged: (selectedIndex) {
+                setRates(selectedIndex: selectedIndex);
+              },
+              children: getPickerItems(),
+            ),
           ),
         ],
       ),
     );
   }
+
+  List<Text> getPickerItems() {
+    List<Text> items = [];
+    for (String currency in currenciesList) {
+      items.add(Text(currency));
+    }
+    return items;
+  }
+
+  void setRates({int selectedIndex}) async {
+    NetWork network = NetWork(currenciesList[selectedIndex]);
+    await network.getRatesFromApi();
+
+    setState(() {
+      selectedPicker = currenciesList[selectedIndex];
+      bitCoinRate = network.btcRate;
+      ethRate = network.ethRate;
+      ltcRate = network.ltcRate;
+    });
+  }
+
 }
